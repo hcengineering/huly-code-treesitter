1e44d5fdcce20126195c740079e79523713e8d60
Nikolay Marchuk <nikolay.marchuk@hardcoreeng.com>
2025-02-21 13:01:30 +0700

    Add specific targetElementEvaluator for TreeSitter

---
+++ src/main/java/com/hulylabs/intellij/plugins/treesitter/editor/TreeSitterTargetElementEvaluator.kt
@@ -0,0 +1,5 @@
+package com.hulylabs.intellij.plugins.treesitter.editor
+
+import com.intellij.codeInsight.TargetElementEvaluatorEx2
+
+class TreeSitterTargetElementEvaluator : TargetElementEvaluatorEx2()
